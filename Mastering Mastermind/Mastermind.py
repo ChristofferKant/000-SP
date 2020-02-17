de0cb0c1a67a2e7c263f0c459af384f41ec0e879
ChristofferAfro <christofferkant@hotmail.nl>
2020-02-17 22:47:25 +0100

    Complete initial push

--- Mastering Mastermind/Mastermind.py
+++ Mastering Mastermind/Mastermind.py
@@ -1,54 +1,90 @@
 import random
-colors = ["[R]ed", "[G]reen", "[B]lue", "[Y]ellow", "[P]urple", "[W]hite"]
+# colors_examples = ["[R]ed", "[G]reen", "[B]lue", "[Y]ellow", "[P]urple", "[W]hite"]
+# colors = ["R", "G", "B", "Y", "P", "W"]
+numbers_examples = [0, 1, 2, 3, 4, 5]
+numbers = [(i, j, k, l) for i in range(0, 6) for j in range(0, 6) for k in range(0, 6) for l in range(0, 6)]
+difficulty_options = ["S", "N", "E"]
+secret = random.choice(numbers)
+
+#def get_secret():
+#	return random.choice(numbers)
+
 
 def main_menu():
 	print("""Welcome to Mastermind.
-Who will be making the code? (ME/AI)""")
+At what difficulty would you like to play?
+Simple means a simple algorithm will try to figure out your code or you will have 10 turns to guess the code.
+Normal means an average algorithm will try to figure out your code or you will have 8 turn to guess the code.
+Expert means an advanced algorithm will (always) figure out your code or you will have 6 turns to guess the code
+Please enter \"S\" for simple, \"N\" for normal or \"E\" for expert)""")
+
+	while True:
+		difficulty = str(input("Enter your choice here: "))
+		if difficulty in difficulty_options:
+			continue
+		print("Please enter \"S\", \"N\" or \"E\"")
+
+	print("Who will be entering the code? (ME/AI)")
 
 	while True:
 		code_maker = str(input("Enter your choice here: "))
 		if code_maker == "ME":
-			player_codemaker()
-			continue
+			player_codemaker(difficulty)
+			break
 		elif code_maker == "AI":
-			ai_codemaker()
-			continue
+			ai_codemaker(difficulty)
+			break
 		print("Please enter \"ME\" or \"AI\"")
 
 
-def player_codemaker():
-	while True:
-		print("""At what difficulty would you like to play?
-You can choose between simple, normal or expert. (S/N/E)
-Simple means a simple algorithm will try to figure out your code.
-Normal means an average algorithm will try to figure out your code.
-Expert means an advanced algorithm will figure out your code.""")
-		difficulty = str(input("Enter your choice here: "))
-		if difficulty == "S" or "N" or "E":
-			break
-		print("Please enter \"S\", \"N\" or \"E\"")
-
-	print("""Choose 4 colors.
-You can choose from """, colors)
-	color_1 = str(input("Color 1 is: "))
-	color_2 = str(input("Color 2 is: "))
-	color_3 = str(input("Color 3 is: "))
-	color_4 = str(input("Color 4 is: "))
-
-	possible_combinations = []
-
-def ai_codemaker():
-	while True:
-		print("""At what difficulty would you like to play?
-You can choose between simple, normal or expert. (S/N/E)
-Simple means you have 10 turns to guess the code.
-Normal means you have 8 turns to guess the code.
-Expert means you have 6 turns to guess the code.""")
-		difficulty = str(input("Enter your choice here: "))
-		if difficulty == "S" or "N" or "E":
-			break
-		print("Please enter \"S\", \"N\" or \"E\"")
-	pass
+def check(guess):
+	black_pegs = 0
+	white_pegs = 0
+	secret_copy = list(secret)
+	i = 0
+	for n in range(len(guess)):
+		if guess[i] == secret[i]:
+			black_pegs += 1
+			secret_copy.remove(guess[i])
+		i += 1
+	i = 0
+	for n in range(len(secret_copy)):
+		if guess[i] in secret_copy:
+			white_pegs += 1
+			secret_copy.remove(guess[i])
+		i += 1
+	print("{} black peg(s),{} white peg(s)".format(black_pegs, white_pegs))
+	return black_pegs, white_pegs
 
 
-player_codemaker()
+def player_codemaker(difficulty):
+	print("Choose 4 numbers. You can choose from", numbers_examples)
+	number_1 = int(input("Number 1 is: "))
+	number_2 = int(input("Number 2 is: "))
+	number_3 = int(input("Number 3 is: "))
+	number_4 = str(input("Number 4 is: "))
+
+
+def ai_codemaker(difficulty):
+	print("""The AI set the code""")
+	if difficulty == "S":
+		max_turns = 11
+	elif difficulty == "N":
+		max_turns = 9
+	elif difficulty == "E":
+		max_turns = 7
+	turn = 1
+	while turn < max_turns:
+		print("Turn", turn)
+		number_1 = int(input("Number 1 is: "))
+		number_2 = int(input("Number 2 is: "))
+		number_3 = int(input("Number 3 is: "))
+		number_4 = str(input("Number 4 is: "))
+		guess = [number_1, number_2, number_3, number_4]
+		check(guess)
+		turn += 1
+		print(secret)
+	print("The secret code is: ", secret)
+
+
+main_menu()
